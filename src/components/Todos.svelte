cdc4a5fd8d9c997055d0b87f6eb80f651283046b
neb03x <neb.3391@gmail.com>
2022-12-12 13:03:26 +0530

    added new changes

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,44 +1,45 @@
-<script> 
-  import { createEventDispatcher } from 'svelte'
-  const dispatch = createEventDispatcher()
-  
-  import FilterButton from './FilterButton.svelte'
-  import Todo from './Todo.svelte'
+<script>
+  import FilterButton from "./FilterButton.svelte";
+  import Todo from "./Todo.svelte";
 
-  export let todos = []
+  export let todos = [];
   $: totalTodos = todos.length;
   $: completedTodos = todos.filter((todo) => todo.completed).length;
 
   function removeTodo(todo) {
-    todos = todos.filter((t) => t.id !== todo.id)
+    todos = todos.filter((t) => t.id !== todo.id);
   }
 
-  let newTodoName = ''
-  $: console.log('newTodoName: ', newTodoName)
+  let newTodoName = "";
+  $: console.log("newTodoName: ", newTodoName);
 
   function addTodo() {
-   todos = [...todos, {id: newTodoId, name: newTodoName, completed: false 
-   }]
-    newTodoName = ''
+    todos = [...todos, { id: newTodoId, name: newTodoName, completed: false }];
+    newTodoName = "";
   }
 
-  let newTodoId 
-     $: {
-        if (totalTodos === 0) {
-            newTodoId = 1;
-        } else {
-            newTodoId = Math.max(...todos.map((t) => t.id)) + 1;
-        }
-     }
+  let newTodoId;
+  $: {
+    if (totalTodos === 0) {
+      newTodoId = 1;
+    } else {
+      newTodoId = Math.max(...todos.map((t) => t.id)) + 1;
+    }
+  }
 
-   let filter = 'all'
-   const filterTodos = (filter, todos) => 
-      filter === 'active' ? todos.filter((t) => !t.completed) :
-      filter === 'completed' ? todos.filter((t) => t.completed) :
-      todos
+  let filter = "all";
+  const filterTodos = (filter, todos) =>
+    filter === "active"
+      ? todos.filter((t) => !t.completed)
+      : filter === "completed"
+      ? todos.filter((t) => t.completed)
+      : todos;
 
-  </script>
-
+  function updateTodo(todo) {
+    const i = todos.findIndex((t) => t.id === todo.id);
+    todos[i] = { ...todos[1], ...todo };
+  }
+</script>
 
 <!-- Todos.svelte -->
 <div class="todoapp stack-large">
@@ -47,35 +48,39 @@
     <h2 class="label-wrapper">
       <label for="todo-0" class="label__lg"> What needs to be done? </label>
     </h2>
-    <input 
-    bind:value={newTodoName}
-    type="text" 
-    id="todo-0" 
-    autocomplete="off" 
-    class="input input__lg"
-     />
+    <input
+      bind:value={newTodoName}
+      type="text"
+      id="todo-0"
+      autocomplete="off"
+      class="input input__lg"
+    />
     <button type="submit" disabled="" class="btn btn__primary btn__lg">
       Add
     </button>
   </form>
 
-  <FilterButton bind:filter={filter} />
+  <FilterButton bind:filter />
 
   <!-- TodosStatus -->
-  <h2 id="list-heading">{completedTodos} out of {totalTodos} items completed</h2>
+  <h2 id="list-heading">
+    {completedTodos} out of {totalTodos} items completed
+  </h2>
 
-
-  
-<ul role="list" class="todo-list stack-large" aria-labelledby="list-heading">
-  {#each filterTodos(filter, todos) as todo (todo.id)}
-  <li class="todo">
-    <Todo {todo} />
-  </li>
-  {:else}
-  <li>Nothing to do here!</li>
-  {/each}
-</ul>
-
+  <!-- svelte-ignore a11y-no-redundant-roles -->
+  <ul role="list" class="todo-list stack-large" aria-labelledby="list-heading">
+    {#each filterTodos(filter, todos) as todo (todo.id)}
+      <li class="todo">
+        <Todo
+          {todo}
+          on:update={(e) => updateTodo(e.detail)}
+          on:remove={(e) => removeTodo(e.detail)}
+        />
+      </li>
+    {:else}
+      <li>Nothing to do here!</li>
+    {/each}
+  </ul>
 
   <hr />
 
